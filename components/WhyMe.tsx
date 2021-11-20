4b70cb5556c39737c7d4045634a54f8ccee4d987
Theo Leveque <theo.leveque@gmail.com>
2021-11-19 22:54:59 -0800

    fixed whyme section layout bug

--- components/WhyMe.tsx
+++ components/WhyMe.tsx
@@ -6,19 +6,19 @@ import WhyMeList from './WhyMeList';
 
 const WhyMe: React.FC<SectionProps> = ({ handleSectionChange }) => {
   return (
-    <InView threshold={0.2} onChange={handleSectionChange}>
+    <InView threshold={0.5} onChange={handleSectionChange}>
       {({ ref }) => (
         <section ref={ref} id='why-me' className='mb-16'>
-          <div className='bg-black py-16 text-center min-h-screen'>
-            <h2 className='text-white '>Why Me?</h2>
+          <div className='bg-black py-16 text-center '>
+            <h2 className='text-white'>Why Me?</h2>
             <WhyMeList />
             {/* Todo: add more details as to why employers should hire me, maybe an interesting story */}
-            <h3 className='text-white'>The Tech I Work With</h3>
+            <h3 className='sm:pb-32 md:pb-16 pb-48 text-white'>
+              The Tech I Work With
+            </h3>
           </div>
-          <div>
-            <div className='relative flex flex-col items-center'>
-              <TechList />
-            </div>
+          <div className='flex flex-col items-center'>
+            <TechList />
           </div>
         </section>
       )}
