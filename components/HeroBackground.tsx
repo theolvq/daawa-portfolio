03706aa270e173126f095d8735562dc533d8f540
Theo Levequet <theo.leveque@gmail.com>
2022-02-05 16:44:06 -0800

    test safari

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -9,7 +9,7 @@ const HeroBackground: React.FC = () => {
       <div className='absolute top-0 left-0 -z-10 hidden lg:block w-full'>
         <Image src={topPattern} alt='' layout='responsive' />
       </div>
-      <div className='absolute -bottom-1 right-0 -z-10 w-full 2xl:w-1/2'>
+      <div className='absolute -bottom-1 right-0 -z-10 w-full hidden lg:block 2xl:w-1/2'>
         <Image src={bottomPattern} alt='' layout='responsive' />
       </div>
     </div>
